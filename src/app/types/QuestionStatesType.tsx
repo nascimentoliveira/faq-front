04161f3ea6d19536948174689a78d4432bc0c1f1
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-07-20 19:56:20 -0300

    feat: create question types

---
+++ src/app/types/QuestionStatesType.tsx
@@ -0,0 +1,7 @@
+export type QuestionStatesType = {
+  openAddQuestionForm: boolean;
+  isExpanded: boolean;
+  editing: boolean;
+  editedQuery: string;
+  editedAnswer: string;
+};
--- src/app/types/QuestionType.tsx
+++ src/app/types/QuestionType.tsx
@@ -1,7 +1,10 @@
-export type Question = {
+export type QuestionType = {
   id: number;
   question: string;
   answer: string | null;
   order: number;
-  subquestions: Question[] | null;
+  parent_question_id: number;
+  createdAt: string;
+  updatedAt: string;
+  subquestions: QuestionType[] | null;
 };
